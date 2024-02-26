acc3146454e86044ec9eb825d9b664e708906c3b
AndyLonly <liujiaqisz@me.com>
2024-02-26 11:55:26 +0000

    Create countdown.js

---
+++ countdown.js
@@ -0,0 +1,21 @@
+function updateCountdown() {
+    const endTime = new Date('2024-03-01T00:00:00'); // Set your event end date here
+    const currentTime = new Date();
+    const timeDifference = endTime - currentTime;
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById('endTime').innerText = endTime.toLocaleString();
+    document.getElementById('timeLeft').innerText = `${days}天 ${hours}小时 ${minutes}分钟 ${seconds}秒`;
+
+    if (timeDifference < 0) {
+        clearInterval(interval);
+        document.getElementById('timeLeft').innerText = '活动已结束';
+    }
+}
+
+updateCountdown();
+const interval = setInterval(updateCountdown, 1000);
