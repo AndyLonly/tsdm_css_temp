39ab5e23ee40f69b91b62cae6ddf05c705696331
AndyLonly <liujiaqisz@me.com>
2024-03-06 16:08:13 +0000

    Update player.js

--- Script/player.js
+++ Script/player.js
@@ -15,5 +15,11 @@ const ap = new APlayer({
         url: 'https://dlink.host/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBdkRlTE5mSUdIc2dndmh3LXIwaUdiS2NoZmhjX3c_ZT02aXVIVG4.mp3',
         cover: './player_cache/unravel.jpg',
 	lrc:'./player_cache/unravel.lrc'
+    },{
+        name: 'beauty is within us',
+        artist: 'Scott Matthew',
+        url: 'https://dlink.host/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBdkRlTE5mSUdIc2dndmh5YmlBUVE4cTUxZDFJNmc_ZT05SDJTOGg.mp3',
+        cover: './player_cache/beauty is within us.jpg',
+	lrc:'./player_cache/beauty is within us.lrc'
     }]
 });
