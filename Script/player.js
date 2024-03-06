9dc063770975ec2f515c7fefa95f4b8623193d7c
AndyLonly <liujiaqisz@me.com>
2024-03-06 16:09:22 +0000

    Update player.js

--- Script/player.js
+++ Script/player.js
@@ -13,13 +13,13 @@ const ap = new APlayer({
         name: 'unravel(acoustic version)',
         artist: 'TK from 凛として時雨',
         url: 'https://dlink.host/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBdkRlTE5mSUdIc2dndmh3LXIwaUdiS2NoZmhjX3c_ZT02aXVIVG4.mp3',
-        cover: './player_cache/unravel.jpg',
-	lrc:'./player_cache/unravel.lrc'
+        cover: './Player_cache/unravel(acoustic version).jpg',
+	lrc:'./Player_cache/unravel.lrc'
     },{
         name: 'beauty is within us',
         artist: 'Scott Matthew',
         url: 'https://dlink.host/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBdkRlTE5mSUdIc2dndmh5YmlBUVE4cTUxZDFJNmc_ZT05SDJTOGg.mp3',
-        cover: './player_cache/beauty is within us.jpg',
-	lrc:'./player_cache/beauty is within us.lrc'
+        cover: './Player_cache/beauty is within us.jpg',
+	lrc:'./Player_cache/beauty is within us.lrc'
     }]
 });
