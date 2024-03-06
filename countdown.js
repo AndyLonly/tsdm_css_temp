8d6b78c77c71fa578c75ff1be0b1614f99bd97eb
AndyLonly <liujiaqisz@me.com>
2024-03-06 16:53:25 +0000

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,6 +1,6 @@
 function updateCountdown() {
     const startTime = new Date('2024-04-01T00:00:00+0800');
-    const endTime = new Date('2024-04-30T00:00:00+08:00');
+    const endTime = new Date('2024-05-01T00:00:00+08:00');
     const currentTime = new Date();
     let timeDifference;
     let timeLeftMessage;
