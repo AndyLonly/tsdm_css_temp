52451643a6d3544ecbf36620c6cf118ab05a7f35
Andy Lonly <liujiaqisz@me.com>
2024-03-09 23:30:54 +0000

    full auto

--- Script/countdown.js
+++ Script/countdown.js
@@ -1,18 +1,34 @@
 function getQueryParameterValue(parameterName, defaultValue) {
     //%2B
+    //?startTime=2024-03-01T00:00:00%2B0800&endTime=2024-04-01T00:00:00%2B08:00
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.has(parameterName) ? new Date(urlParams.get(parameterName)) : new Date(defaultValue);
 }
 
 function updateCountdown() {
-    const startTime = getQueryParameterValue('startTime', '2024-04-01T00:00:00+0800');
-    const endTime = getQueryParameterValue('endTime', '2024-05-01T00:00:00+08:00');
+    const startTime = getQueryParameterValue('startTime', '2024-01-01T00:00:00+0800');
+    const endTime = getQueryParameterValue('endTime', '2024-02-01T00:00:00+08:00');
     const currentTime = new Date();
+    const options = {
+        timeZone: 'Asia/Shanghai',
+        hour12: false ,
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+    };
     let timeDifference;
     let timeLeftMessage;
 
-    document.getElementById('startTime').innerText = `活动开始时间: ${startTime.toLocaleString()}`;
-    document.getElementById('endTime').innerText = `活动结束时间: ${endTime.toLocaleString()}`;
+    const startTimeElement = document.getElementById('startTime');
+    const endTimeElement = document.getElementById('endTime');
+
+    startTimeElement.parentNode.title = `提示：已转换至本地时间，原本时间为${startTime.toLocaleString('zh-CN', options)}[+8时区/24小时制]`;
+    endTimeElement.parentNode.title = `提示：已转换至本地时间，原本时间为${endTime.toLocaleString('zh-CN', options)}[+8时区/24小时制]`;
+
+    startTimeElement.innerText = `活动开始时间: ${startTime.toLocaleString()}`;
+    endTimeElement.innerText = `活动结束时间: ${endTime.toLocaleString()}`;
 
     if (currentTime < startTime) {
         // 活动尚未开始
